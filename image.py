d397e3c0918002988c9fccd8a3a998cbdd36bbc5
Geusen <89836078+Geusen@users.noreply.github.com>
2022-05-02 15:53:07 +0900

    Update and rename schedule.py to image.py

---
+++ image.py
@@ -0,0 +1,53 @@
+import shutil
+import os
+import time
+import requests
+import cv2 
+import numpy as np
+import pprint
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+from PIL import Image
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+#-----------------------------------------------------------------------------
+# Chromeヘッドレスモード起動
+options = webdriver.ChromeOptions()
+options.headless = True
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
+driver = webdriver.Chrome('chromedriver',options=options)
+driver.implicitly_wait(10)
+
+# 対象URL(伏せています)
+url = 'https://docs.google.com/spreadsheets/d/128mFFj6w1drdDzxsKsoc9Xdba2FEWSffcM2iUSrnZ0c/edit#gid=0'
+
+# ウインドウ幅、高さ指定
+windowSizeWidth = 680
+windowSizeHeight = 700
+
+# サイトURL取得
+driver.get(url)
+WebDriverWait(driver, 15).until(EC.presence_of_all_elements_located)
+  
+# ウインドウ幅・高さ指定
+windowWidth = windowSizeWidth if windowSizeWidth else driver.execute_script('return document.body.scrollWidth;')
+windowHeight = windowSizeHeight if windowSizeHeight else driver.execute_script('return document.body.scrollHeight;')
+driver.set_window_size(windowWidth, windowHeight)
+
+# スクリーンショット格納
+driver.save_screenshot('before.png')
+
+# サーバー負荷軽減処理
+time.sleep(1)
+
+# ブラウザ稼働終了
+driver.quit()
+
+# 画像トリミング
+im = Image.open('before.png')
+im.crop((35, 145, 640, 645)).save('now.png', quality=95)
